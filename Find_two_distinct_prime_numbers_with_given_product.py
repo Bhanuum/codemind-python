1f619e9b65e4fa954a0028c0f1ca2ffd76735b0f
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-06-30 18:27:35 +0530

    def prime(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True n=int(input()) c=0 for i in range(1,n):     for j in range(i,n):         if (prime(i) and prime(j)):             if i<j:                 if i*j==n:                     print(i,j)                     c=1                     break if c==0:     print("-1")

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -1,17 +1,17 @@
 def prime(n):
     for i in range(2,int(n**0.5)+1):
         if n%i==0:
-            break
-    return n
+            return False
+    return True
 n=int(input())
-f=0
+c=0
 for i in range(1,n):
     for j in range(i,n):
         if (prime(i) and prime(j)):
-            if i<j and i*j==n:
-                print(i,j)
-                f+=1
-                break
-if f==0:
+            if i<j:
+                if i*j==n:
+                    print(i,j) 
+                    c=1
+                    break
+if c==0:
     print("-1")
-        
