8f33fb72c3df9a3f11797597c1bf12c589d86ae4
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-05 15:55:14 +0530

    n=input() c=[] for i in n:     if i in "aeiouAEIOU":         if i not in c:           c.append(i) print(*c)

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,10 +1,7 @@
-s=input()
-c=0
-p=[]
-for i in s:
-    if i in "AEIOUaeiou":
-        if i not in p:
-            p.append(i)
-for i in p:            
-  print(i,end=" ")        
-        
+n=input()
+c=[]
+for i in n:
+    if i in "aeiouAEIOU":
+        if i not in c:
+          c.append(i)
+print(*c)        
