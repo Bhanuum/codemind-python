ceab29261eed9bf134615f94c25e6e652b9d60c3
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-07 22:27:45 +0530

    a=input().lower() c=[] for i in a:     if i not in c and i!=" ":         c.append(i) if len(c)==26:     print(True) else:     print(False)

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,11 +1,10 @@
-n=input().lower()
-s="abcdefghijklmnopqrstuvwxyz"
-p=[]
-for i in n:
-    if i in s and i!=" ":
-        if i not in p:
-            p.append(i)
-if len(p)==26:
+a=input().lower()
+c=[]
+for i in a:
+    if i not in c and i!=" ":
+        c.append(i)
+if len(c)==26:
     print(True)
 else:
     print(False)
+  
