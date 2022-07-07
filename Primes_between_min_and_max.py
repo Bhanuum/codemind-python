05d625efd90da2af1eec88fc62a174e09025eb00
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-07 20:14:05 +0530

    def prime(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True n=int(input()) p=list(map(int,input().split())) m=p.index(min(p)) n=p.index(max(p)) c=0 if m<n:     for i in range(m,n+1):         if prime(p[i]):           c+=1     print(c) else:     for i in range(n,m+1):         if prime(p[i]):             c+=1     print(c)

--- Primes_between_min_and_max.py
+++ Primes_between_min_and_max.py
@@ -4,19 +4,19 @@ def prime(n):
     for i in range(2,int(n**0.5)+1):
         if n%i==0:
             return False
-    return True        
+    return True
 n=int(input())
 p=list(map(int,input().split()))
-s=p.index(min(p))
-d=p.index(max(p))
+m=p.index(min(p))
+n=p.index(max(p))
 c=0
-if s<d:
- for i in range(s,d+1):
+if m<n:
+    for i in range(m,n+1):
         if prime(p[i]):
-            c+=1 
- print(c)
+          c+=1
+    print(c)     
 else:
-     for i in range(d,s+1):
+    for i in range(n,m+1):
         if prime(p[i]):
             c+=1
-     print(c)
+    print(c)
