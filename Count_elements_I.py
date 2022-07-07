770636638d85040bd5cfd4016678ecf18bf30b3f
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-07 20:27:22 +0530

    a,b=map(int,input().split()) p=list(map(int,input().split())) k=list(map(int,input().split())) c=[] for i in range(0,a):     for j in range(0,b):         if p[i] not in c:           if p[i]==k[j]:             c.append(p[i]) print(len(c))

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -4,7 +4,7 @@ k=list(map(int,input().split()))
 c=[]
 for i in range(0,a):
     for j in range(0,b):
-        if p[i]==k[j]:
-          if p[i] not in c:
-              c.append(p[i])
-print(len(c))         
+        if p[i] not in c:
+          if p[i]==k[j]:
+            c.append(p[i])
+print(len(c))            
