1852847aff7ea407991d68075c15cafb3fe3dca6
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-07 22:43:54 +0530

    n=int(input()) k=list(map(int,input().split())) p=[] for i in range(0,n,2):     j=0     while j<k[i+1]:         p.append(k[i])         j+=1 print(*p)

--- Generate_array_I.py
+++ Generate_array_I.py
@@ -1,9 +1,9 @@
-n = int(input())
-p = list(map(int,input().split()))
-c = []
+n=int(input())
+k=list(map(int,input().split()))
+p=[]
 for i in range(0,n,2):
-    j=1
-    while j<=p[i+1]:
-        c.append(p[i])
+    j=0
+    while j<k[i+1]:
+        p.append(k[i])
         j+=1
-print(*c)        
+print(*p)        
