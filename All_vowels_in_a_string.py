d1f2ef154a3feadfe886d74e7d2b6c13f232ecc0
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-07 21:34:21 +0530

    n=input() p="aeiou" k="AEIOU" c=0 j=0 for i in p:     if i in n:         c+=1 for i in k:     if i in n:         j+=1 if c==5 or j==5:     print(True) else:     print(False)

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,17 +1,15 @@
 n=input()
-a="aeiou"
-b="AEIOU"
-p=[]
-f=[]
-for i in n:
-    if i in a:
-      if i not in p:
-          p.append(i)
-for i in n:
-    if i in b:
-      if i not in p:
-          f.append(i)
-if len(p)==5 or len(f)==5:
+p="aeiou"
+k="AEIOU"
+c=0
+j=0
+for i in p:
+    if i in n:
+        c+=1
+for i in k:
+    if i in n:
+        j+=1
+if c==5 or j==5:
     print(True)
 else:
     print(False)
