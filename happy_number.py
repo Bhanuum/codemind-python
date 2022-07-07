4a75d5640775ecb294c2253e1583e17c55299f42
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-07 13:51:17 +0530

    n=int(input()) p=0 while True:  while n:     r=n%10     n//=10     p+=r**2  if p==1 or p==7:         print(True)         break  elif p<10:         print(False)         break  else:         n=p         p=0

--- happy_number.py
+++ happy_number.py
@@ -1,15 +1,16 @@
-n = int(input())
-sum = 0
+n=int(input())
+p=0
 while True:
-    while n!=0:
-        sum+=(n%10)**2
-        n//=10
-    if sum==1 or sum==7:
+ while n:
+    r=n%10
+    n//=10
+    p+=r**2
+ if p==1 or p==7:
         print(True)
         break
-    elif sum<10:
+ elif p<10:
         print(False)
         break
-    else:
-        n=sum
-        sum=0
+ else:
+        n=p
+        p=0
