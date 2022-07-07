5fa9fa8406357376155355bd2d31439a17af926b
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-07 21:15:17 +0530

    n=int(input()) p=list(map(int,input().split())) c=0 for i in range(0,n-1):     if p[i]<p[i+1]:         pass     else:         c+=1         break if c==0:     print("yes") else:     print("no")

--- Ascending_array.py
+++ Ascending_array.py
@@ -1,10 +1,14 @@
 n=int(input())
-k=list(map(int,input().split()))
+p=list(map(int,input().split()))
 c=0
 for i in range(0,n-1):
-    if k[i]<k[i+1]:
+    if p[i]<p[i+1]:
+        pass
+    else:
         c+=1
-if c==n-1:
+        break
+if c==0:
     print("yes")
 else:
     print("no")
+  
