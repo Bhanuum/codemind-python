6a486f3a38a4c8f73cc3796cd61d9c4fe59fb947
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-08 14:12:12 +0530

    n=input().lower() m=input().lower() c="" for i in m:     if i not in n:         if i not in c:           c+=i for i in n:     if i not in m:         if i not in c:          c+=i p=sorted(c) p=str(p) p=p.replace(" ","") p=p.replace("]","") p=p.replace("[","") p=p.replace(",","") p=p.replace("'","") print(p)

---
+++ non_common_characters.py
@@ -0,0 +1,19 @@
+n=input().lower()
+m=input().lower()
+c=""
+for i in m:
+    if i not in n:
+        if i not in c:
+          c+=i
+for i in n:
+    if i not in m:
+        if i not in c:
+         c+=i
+p=sorted(c)
+p=str(p)
+p=p.replace(" ","")
+p=p.replace("]","")
+p=p.replace("[","")
+p=p.replace(",","")
+p=p.replace("'","")
+print(p)
