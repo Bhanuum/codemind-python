d456d312cf9cb407f0baa1693b61ed0008271a66
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-08 14:58:44 +0530

    n=int(input()) for i in range(n):     p=int(input())     s=input()     c=0     for i in s:         if s.count(i)==1:             print(i)             c+=1             break     if c==0:         print("-1")

---
+++ First_Non_Repeated_Character.py
@@ -0,0 +1,12 @@
+n=int(input())
+for i in range(n):
+    p=int(input())
+    s=input()
+    c=0
+    for i in s:
+        if s.count(i)==1:
+            print(i)
+            c+=1
+            break
+    if c==0:
+        print("-1")
