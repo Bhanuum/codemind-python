e93102c8485d41f0d348f20407dddc87a02981fa
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-10 23:12:01 +0530

    n,m=map(int,input().split()) b=[] for i in range(n):     p=list(map(int,input().split()))     b.append(p) v=0 for i in range(n):     s=0     for j in range(m):         s+=b[i][j]     print(s,end=" ")     s=0

---
+++ Row_wise_sum_of_a_matrix.py
@@ -0,0 +1,12 @@
+n,m=map(int,input().split())
+b=[]
+for i in range(n):
+    p=list(map(int,input().split()))
+    b.append(p)
+v=0
+for i in range(n):
+    s=0
+    for j in range(m):
+        s+=b[i][j]
+    print(s,end=" ")
+    s=0
