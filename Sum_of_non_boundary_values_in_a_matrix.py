4246d5c21e11c0c41994b2865fb4f126f2e9ba36
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-10 23:35:26 +0530

    n,m = map(int,input().split()) b = [] x = 0 for i in range(n):     a = list(map(int,input().split()))     b.append(a) s=0 for i in range(n):     for j in range(m):         if i==0 or i==n-1 or j==0 or j==m-1:             pass         else:             s+=b[i][j] print(s)

---
+++ Sum_of_non_boundary_values_in_a_matrix.py
@@ -0,0 +1,14 @@
+n,m = map(int,input().split())
+b = []
+x = 0
+for i in range(n):
+    a = list(map(int,input().split()))
+    b.append(a)
+s=0    
+for i in range(n):
+    for j in range(m):
+        if i==0 or i==n-1 or j==0 or j==m-1:
+            pass
+        else:
+            s+=b[i][j]
+print(s)            
