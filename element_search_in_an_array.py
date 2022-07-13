0032e8bf3f2da81a984be1342ccb95887f20cf34
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-13 21:01:53 +0530

    n= int(input()) arr = list(map(int,input().split())) b=int(input()) c=0 for i in arr:     if b in arr:         c+=1 if c>0:     print(True) else:     print(False)

--- element_search_in_an_array.py
+++ element_search_in_an_array.py
@@ -1,14 +1,11 @@
-n=int(input())
-k=list(map(int,input().split()))
-t=int(input())
+n= int(input())
+arr = list(map(int,input().split()))
+b=int(input())
 c=0
-for i in range(len(k)):
-      if k[i]==t:
-          c=1
-if c==1:
-    print("True")
+for i in arr:
+    if b in arr:
+        c+=1
+if c>0:
+    print(True)
 else:
-    print("False")
-          
-          
-          
+    print(False)
