50eaafe25246f55ae6cae8ab041cf199bc0e621c
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-13 20:38:30 +0530

    n=int(input()) p=list(map(int,input().split())) c=0 k=[] for i in range(n):     if p[i]%2==0:         k.append(p[i]) for i in range(0,n):    if p[i]%2==0:         if i%2==0:           c+=1 if len(k)==c:     print(True) else:     print(False)

---
+++ Strictly_EVEN.py
@@ -0,0 +1,16 @@
+n=int(input())
+p=list(map(int,input().split()))
+c=0
+k=[]
+for i in range(n):
+    if p[i]%2==0:
+        k.append(p[i])
+for i in range(0,n):
+   if p[i]%2==0:
+        if i%2==0:
+          c+=1
+if len(k)==c:
+    print(True)
+else:
+    print(False)
+   
