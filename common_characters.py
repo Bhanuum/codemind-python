23c70d55f1a0e7f587236282b8f625e467d4dcc6
Bhanuum <101499789+Bhanuum@users.noreply.github.com>
2022-07-14 11:19:34 +0530

    m = input().lower() n = input().lower() a = '' for i in m:     if i in n and i not in a and i!=" ":         a+=i a=sorted(a) a = str(a) a = a.replace("[","") a = a.replace("]","") a = a.replace("'","") a = a.replace(" ","") a = a.replace(",","") if len(a)==0:     print("-1") else:     print(a)

--- common_characters.py
+++ common_characters.py
@@ -1,22 +1,17 @@
-n=input().lower()
-m=input().lower()
-c=""
+m = input().lower()
+n = input().lower()
+a = ''
 for i in m:
-    if i  in n:
-        if i not in c:
-          c+=i
-for i in n:
-    if i in m:
-        if i not in c:
-         c+=i
-p=sorted(c)
-p=str(p)
-p=p.replace(" ","")
-p=p.replace("]","")
-p=p.replace("[","")
-p=p.replace(",","")
-p=p.replace("'","")
-if len(c)==0:
+    if i in n and i not in a and i!=" ":
+        a+=i
+a=sorted(a)
+a = str(a)
+a = a.replace("[","")
+a = a.replace("]","")
+a = a.replace("'","")
+a = a.replace(" ","")
+a = a.replace(",","")
+if len(a)==0:
     print("-1")
 else:
-    print(p)
+    print(a)
